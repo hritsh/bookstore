8595d120396f5c60ca8de4b411b37bec709cc68e
hritsh <hritish.mahajan@gmail.com>
2023-04-24 01:14:48 +0400

    Create Python Interface for Customer and Admin

---
+++ bookstore.py
@@ -0,0 +1,273 @@
+import mysql.connector
+"""
+CREATE TABLE Book (
+  ISBN CHAR(13) PRIMARY KEY,
+  Title VARCHAR(255) NOT NULL,
+  Author VARCHAR(255) NOT NULL,
+  Publisher VARCHAR(255) NOT NULL,
+  Publication_Date DATE NOT NULL,
+  Price DECIMAL(10, 2) NOT NULL,
+  Quantity INT NOT NULL
+);
+
+CREATE TABLE Customer (
+  Customer_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  Phone VARCHAR(20) NOT NULL,
+  Address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `Order` (
+  Order_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Customer_ID INT NOT NULL,
+  Order_Date DATETIME NOT NULL,
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
+);
+
+CREATE TABLE Transaction (
+  Transaction_ID INT AUTO_INCREMENT PRIMARY KEY,
+  Order_ID INT NOT NULL,
+  Transaction_Date DATETIME NOT NULL,
+  Payment_Method VARCHAR(255) NOT NULL,
+  FOREIGN KEY (Order_ID) REFERENCES `Order`(Order_ID)
+);
+
+CREATE TABLE Order_Item (
+  Order_ID INT NOT NULL,
+  ISBN CHAR(13) NOT NULL,
+  Quantity INT NOT NULL,
+  Price DECIMAL(10, 2) NOT NULL,
+  PRIMARY KEY (Order_ID, ISBN),
+  FOREIGN KEY (Order_ID) REFERENCES `Order`(Order_ID),
+  FOREIGN KEY (ISBN) REFERENCES Book(ISBN)
+);"""
+
+
+def tableprint(lst):
+    widths = []
+    columns = []
+    border = '|'
+    separator = '+'
+    length = ['' for i in range(len(lst[0]))]
+    for i in range(len(lst[0])):
+        length[i] = max(list(map(lambda x: len(str(x[i])), lst)))
+
+    i = 0
+    for cd in cursor.description:
+        widths.append(max(length[i], len(cd[0])))
+        columns.append(cd[0])
+        i += 1
+
+    for w in widths:
+        border += " %-"+"%ss |" % (w,)
+        separator += '-'*w + '--+'
+
+    print(separator)
+    print(border % tuple(columns))
+    print(separator)
+    for row in lst:
+        print(border % row)
+    print(separator)
+
+
+# Connect to the MySQL database
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="bookstore"
+)
+
+# Create a cursor object
+cursor = connection.cursor()
+cart = {}
+
+# Create a menu
+
+
+def customer_menu():
+    while True:
+        print("What would you like to do?")
+        print("1. Show all books")
+        print("2. Search for a book")
+        print("3. Add to cart")
+        print("4. Check out")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            # Show all books
+            cursor.execute("SELECT * FROM Book")
+            books = cursor.fetchall()
+            tableprint(books)
+
+        elif choice == "2":
+            # Search for a book
+            isbn = input("Enter the ISBN of the book you want to search for: ")
+            cursor.execute("SELECT * FROM Book WHERE ISBN=%s" % isbn)
+            book = cursor.fetchone()
+            if book is None:
+                print("Book not found")
+            else:
+                print("ISBN:", book[0])
+                print("Title:", book[1])
+                print("Author:", book[2])
+                print("Publisher:", book[3])
+                print("Publication Date:", book[4])
+                print("Price:", book[5])
+                print("Quantity:", book[6])
+
+        elif choice == "3":
+            # Add to cart
+            isbn = input(
+                "Enter the ISBN of the book you want to add to cart: ")
+            quantity = input(
+                "Enter the quantity of the book you want to add to cart: ")
+
+            cursor.execute("SELECT * FROM Book WHERE ISBN=%s" % isbn)
+            book = cursor.fetchone()
+            if book is None:
+                print("Book not found")
+            else:
+                if int(quantity) > int(book[6]):
+                    print("Quantity not available")
+                else:
+                    cart[isbn] = quantity
+
+            print("Book added to cart")
+
+        elif choice == "4":
+            # Check out
+            customer_name = input("Enter your name: ")
+            customer_email = input("Enter your email address: ")
+            customer_phone = input("Enter your phone number: ")
+            customer_address = input("Enter your address: ")
+            cursor.execute("INSERT INTO Customer (Name, Email, Phone, Address) VALUES ('%s', '%s', '%s', '%s')" % (
+                customer_name, customer_email, customer_phone, customer_address))
+            connection.commit()
+            customer_id = cursor.lastrowid
+            cursor.execute(
+                "INSERT INTO `Order` (Customer_ID, Order_Date) VALUES ( %s, now())" % (customer_id))
+            connection.commit()
+            order_id = cursor.lastrowid
+            for book in cart:
+                cursor.execute("INSERT INTO OrderItem (Order_ID, ISBN, Quantity) VALUES (%s, '%s', %s)" % (
+                    order_id, book, cart[book]))
+                connection.commit()
+                cursor.execute(
+                    "INSERT INTO Transaction (Order_ID, Transaction_Date, Payment_Method) VALUES (%s, now(), 'Cash')" % (order_id))
+                cursor.execute("UPDATE Book SET Quantity = Quantity - %s WHERE ISBN = %s" % (
+                    quantity, isbn))
+                connection.commit()
+                books.append(
+                    (isbn, book[1], book[2], book[3], book[4], book[5], quantity))
+            print("Order placed successfully")
+
+        elif choice == "5":
+            # Exit
+            break
+
+        else:
+            print("Invalid choice")
+
+# Create a menu
+
+
+def admin_menu():
+    while True:
+        print("What would you like to do?")
+        print("1. Show all books")
+        print("2. Add a book")
+        print("3. Update a book")
+        print("4. Delete a book")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            # Show all books
+            cursor.execute("SELECT * FROM Book")
+            books = cursor.fetchall()
+            tableprint(books)
+
+        elif choice == "2":
+            # Add a book
+            isbn = input("Enter the ISBN of the book: ")
+            title = input("Enter the title of the book: ")
+            author = input("Enter the author of the book: ")
+            publisher = input("Enter the publisher of the book: ")
+            publication_date = input(
+                "Enter the publication date of the book: ")
+            price = input("Enter the price of the book: ")
+            quantity = input("Enter the quantity of the book: ")
+
+            cursor.execute("INSERT INTO Book (ISBN, Title, Author, Publisher, Publication_Date, Price, Quantity) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (
+                isbn, title, author, publisher, publication_date, price, quantity))
+            connection.commit()
+            print("Book added successfully")
+
+        elif choice == "3":
+            # Update a book
+            isbn = input("Enter the ISBN of the book: ")
+            title = input("Enter the title of the book: ")
+            author = input("Enter the author of the book: ")
+            publisher = input("Enter the publisher of the book: ")
+            publication_date = input(
+                "Enter the publication date of the book: ")
+            price = input("Enter the price of the book: ")
+            quantity = input("Enter the quantity of the book: ")
+
+            cursor.execute("UPDATE Book SET Title='%s', Author='%s', Publisher='%s', Publication_Date='%s', Price='%s', Quantity='%s' WHERE ISBN='%s'" % (
+                title, author, publisher, publication_date, price, quantity, isbn))
+            connection.commit()
+            print("Book updated successfully")
+
+        elif choice == "4":
+            # Delete a book
+            isbn = input("Enter the ISBN of the book: ")
+
+            cursor.execute("DELETE FROM Book WHERE ISBN='%s'" % isbn)
+            connection.commit()
+            print("Book deleted successfully")
+
+        elif choice == "5":
+            # Exit
+            break
+
+        else:
+            print("Invalid choice")
+
+
+# Create a main menu
+def menu():
+    while True:
+        print("What would you like to login as?")
+        print("1. Customer")
+        print("2. Admin")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            # Customer
+            customer_menu()
+
+        elif choice == "2":
+            # Admin
+            admin_menu()
+
+        elif choice == "3":
+            # Exit
+            break
+
+        else:
+            print("Invalid choice")
+
+
+# Close the cursor and connection
+if __name__ == "__main__":
+    menu()
+    cursor.close()
+    connection.close()
